db0380e9bba1fa2b013c0542a3ccfead0c88df38
Yusuf Dönmez <yusuf.donmez@hey.com>
2021-02-20 22:34:53 +0300

    Add files via upload

---
+++ akif-personal-website/pages/index.js
@@ -0,0 +1,42 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className="flex justify-center">
+      <Head>
+        <title>Mehmet Akif Özkanoğlu</title>
+      </Head>
+
+      <div className="mt-4 mb-4 md:mr-40 sm:mr-2 p-4 md:w-4/6 sm:w-5/6 rounded text-left text-base md:text-2xl sm:text-6xl">
+        <p className="mt-8 text-4xl">Hi.</p>
+        <p className="mt-4 text-gray-800 sm:text-xl">My name is Mehmet Akif Özkanoğlu. I am a full time researcher pursuing a master of science on Computer Vision. My research interest is Object Detection, Tracking, SLAM for autononomous system and UAVs.</p>
+        <p className="mt-8 text-gray-800">
+          Feel free to contact me via: <a className="text-blue-400" href="mailto:makifozkanoglu@gmail.com">makifozkanoglu@gmail.com</a>
+        </p>
+        <p className="mt-12 text-red-900 text-3xl">
+          Education
+        </p>
+        <p className="mt-6 text-blue-800">
+          Bilkent University
+        </p>
+        <p className="mt-2 text-gray-800 md:text-xl">
+          MSc, Computer Engineering - 2022 (Expected)
+        </p>
+        <p className="mt-6 text-blue-800">
+          Istanbul Technical University
+        </p>
+        <p className="mt-2 text-gray-800 md:text-xl">
+          BSc, Electronic & Communication Engineering - 2020 
+        </p>
+        <p className="mt-12 text-red-900 text-3xl">
+          Resources
+        </p>
+        <p className="mt-6">
+          <a className="text-blue-400" href="https://www.linkedin.com/in/makifozkanoglu/">LinkedIn</a>
+          <a className="ml-4 text-grey-900" href="https://github.com/makifozkanoglu">GitHub</a>
+        </p>
+      </div>
+    </div>
+  )
+}
