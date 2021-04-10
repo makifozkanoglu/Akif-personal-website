fbd9c878cd09c5c2d5fad922ee7bcf3df8c77ac2
makifozkanoglu <makifozkanoglu@gmail.com>
2021-04-11 01:26:08 +0300

    rss

--- akif-personal-website/postcss.config.js
+++ akif-personal-website/postcss.config.js
@@ -4,13 +4,3 @@ module.exports = {
     autoprefixer: {},
   },
 }
-
-module.exports = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      require('./lib/sitemap');
-    }
-
-    return config;
-  }
-};
